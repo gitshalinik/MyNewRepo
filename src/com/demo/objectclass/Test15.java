ce2ffc545182c131f623707d33ccda2286ec582f
SHALINIK <SHALINIK@SHALINIK-7410.in.oracle.com>
2023-04-25 17:14:26 +0530

    added Test15.java

---
+++ src/com/demo/objectclass/Test15.java
@@ -0,0 +1,29 @@
+package com.demo.objectclass;
+
+
+public class Test15
+{
+	public static void main(String[] args)   
+	{
+		try
+		{
+			String s1 = null;
+			System.out.println(s1.length());
+		}
+		catch(NullPointerException e)
+		{
+			e.printStackTrace();
+		}
+		catch(ArithmeticException e)
+		{
+			e.printStackTrace();
+		}
+	
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		System.out.println("Other code: ");	
+	}
+}
